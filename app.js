3dad28d6bfe9c3d176db92aa32488d04ee001c4a
karthicrajakumar <redrageclan@gmail.com>
2016-09-24 09:46:33 +0530

    second

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose  = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/helpme');
+var app = express();
 var routes = require('./routes/index');
 var jwt    = require('jsonwebtoken');
 var User = require('./app/models/user');
