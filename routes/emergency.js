1a746f9d3f475d213effa1ac5b0942f11ae5d30a
karthicrajakumar <redrageclan@gmail.com>
2016-09-24 13:25:46 +0530

    second

--- routes/emergency.js
+++ routes/emergency.js
@@ -2,18 +2,32 @@ var express = require('express');
 var router = express.Router();
 var User = require('../app/models/user');
 var Community = require('../app/models/community');
-
+var mongoose = require('mongoose');
 
 router.post('/',function(req,res){
     var id = req.decoded;
     var home = req.body.home;
     var lat = req.body.lat;
     var long = req.body.long;
+    var io = req.io;
     
-    var community;
+    
     User.find({'_id':mongoose.Types.ObjectId(id)},function(err,user){
-        community = user.community;
-    })
+      var io = req.io
+      
+      var doc = {
+        user: user,
+        lat: lat,
+        long : long
+      }
+      
+      console.log(doc);
+      io.sockets.emit(home+"_emergency",{result:doc}); // how?
+
+    });
+    return res.json({success: true, message:"Success" });
+    
+    
     
 });
 
